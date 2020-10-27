505f02aed4d4a4963c6674d9c0be81314e12e8b7
Anthony Giles <omgliekpie@gmail.com>
2020-10-27 13:12:55 -0600

    start planning project by putting the specifications in comments

---
+++ lib/hangman.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# When a game is started load the dictionary and randomly select a word between
+# 5 and 12 characters long to be the secret word.
+
+# Display a count that shows the how many incorrect guesses are left before the
+# game will end. Also display the correct letters that have already been choosen
+# and their position in the hidden word along with any that have already been
+# choosen. (ex. r_b_)
+
+# Each turn have the player guess a letter (case insensitive) and then display
+# whether it was correct or not. If the player is out of guesses then they loose
+# they game.
+
+# Implement the ability to save the game at the start of the player's turn.
+
+# When the program is openeed add an option to load one of your saved games.
